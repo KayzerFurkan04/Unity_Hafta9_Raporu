8e7c02e82772ba986e400409958b28304f018217
Ahmet Furkan Öcel <157826103+KayzerFurkan04@users.noreply.github.com>
2024-12-18 23:05:12 +0300

    Unity_Hafta9_Raporu

--- AsteroidScript.cs
+++ AsteroidScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class AsteroidScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float rotateSpeed = 20f;
+
+    public GameObject explosionPrefab;
+    public spawnmanagerscript spawnmanagerscript;
+
     void Start()
     {
-        
+        spawnmanagerscript = GameObject.Find("spawnmanagerscript").GetComponent<spawnmanagerscript>();
+        if (spawnmanagerscript == null) Debug.LogError("Asteroid_sc::Start spawnmanagerscript is NULL");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            spawnmanagerscript.StartSpawning();
+            Destroy(this.gameObject);
+        }
     }
 }
--- ExplosionScript.cs
+++ ExplosionScript.cs
@@ -4,31 +4,13 @@ using UnityEngine;
 
 public class ExplosionScript : MonoBehaviour
 {
-    float rotateSpeed = 20f;
-
-    [SerializeField]
-    GameObject explosionPrefab;
-    spawnmanagerscript spawnmanagerscript;
-
     void Start()
     {
-        spawnmanagerscript = GameObject.Find("spawnmanagerscript").GetComponent<spawnmanagerscript>();
-        if (spawnmanagerscript == null) Debug.LogError("Asteroid_sc::Start spawnmanagerscript is NULL");
+        Destroy(this.gameObject, 3.0f);
     }
 
     void Update()
     {
-        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Laser")
-        {
-            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            Destroy(other.gameObject);
-            spawnmanagerscript.StartSpawning();
-            Destroy(this.gameObject);
-        }
+        
     }
 }
